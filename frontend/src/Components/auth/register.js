07e2284f0db407df6fad7b54217816f0f4aa6b90
Ayoubouladmaalem <ouladmaalem.ayoub@etu.uae.ac.ma>
2024-12-22 20:12:23 +0100

    push connected api authentifications

--- frontend/src/Components/auth/register.js
+++ frontend/src/Components/auth/register.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import './register.css';
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Register() {
-    const [formData, setFormData] = useState({nom: "",prenom: "",sexe: "",telephone: "",birthDate: "",poids: "",email: "",password: "",confirmPassword: ""});
+    const [formData, setFormData] = useState({nom: "",prenom: "",sexe: "",telephone: "",dateNaissance: "",poids: "",email: "",motDePasse: "",confirmationMotDePasse: ""});
     const [errorMessage, setErrorMessage] = useState("")
-
+    const navigate = useNavigate(); 
     const handleDateChange = (e) => {
-        setFormData({ ...formData, birthDate: e.target.value });
+        setFormData({ ...formData, dateNaissance: e.target.value });
     };
 
     const handleChange =(e)=>{
@@ -15,15 +16,21 @@ function Register() {
         setFormData({...formData, [id]:value});
     }
 
+    const formattedData = { ...formData, poids: parseFloat(formData.poids) };
     const handleSubmit = async(e)=>{
         e.preventDefault();
-        if(formData.password !== formData.confirmPassword){
+        if(formData.motDePasse !== formData.confirmationMotDePasse){
             setErrorMessage("les deux mots de passe ne sont pas identiques")
         }
+        if (!formData.poids || isNaN(formData.poids) || formData.poids <= 0) {
+            setErrorMessage("Le poids doit être un nombre supérieur à 0");
+            return;
+        }
         try{
-            await axios.post("http://localhost:8080/api/register", formData);
+            await axios.post("http://localhost:8080/auth/register", formattedData);
             console.log("l'utilisateur est bien authentifié");
-            setFormData({ nom: "",prenom: "",sexe: "",telephone: "",birthDate: "",poids: "",email: "",password: "",confirmPassword: ""});
+            navigate("/login");
+            setFormData({ nom: "",prenom: "",sexe: "",telephone: "",dateNaissance: "",poids: "",email: "",motDePasse: "",confirmationMotDePasse: ""});
         }catch(error){
             console.error("smtg maaachi hia hadek", error);
         }
@@ -47,19 +54,29 @@ function Register() {
                         </div>
 
                         <div className="input-group mb-3">
-                            <input type="date" className="form-control me-2" id="birthDate" value={formData.birthDate}
+                            <input type="date" className="form-control me-2" id="dateNaissance" value={formData.dateNaissance}
                                 onChange={handleDateChange} min="1940-01-01" max="2018-12-31" aria-label="Date de naissance"/>
-                            <input type="text" className="form-control" placeholder="Poids en kg" aria-label="Poids"  onChange={handleChange}/>
+                            <input 
+                                type="number" 
+                                className="form-control" 
+                                placeholder="Poids en kg" 
+                                aria-label="Poids" 
+                                id="poids" 
+                                value={formData.poids} 
+                                onChange={handleChange} 
+                                required 
+                                min="1" 
+                            />                        
                         </div>
 
                         <div className="form-group mb-3">
                             <input type="email" className="form-control" placeholder="Entrer email" id="email" value={formData.email} onChange={handleChange}/>
                         </div>
                         <div className="form-group mb-3">
-                            <input type="password" className="form-control" placeholder="Mot de passe" id="password" value={formData.password} onChange={handleChange}/>
+                            <input type="password" className="form-control" placeholder="Mot de passe" id="motDePasse" value={formData.motDePasse} onChange={handleChange}/>
                         </div>
                         <div className="form-group mb-3">
-                            <input type="password" className="form-control " placeholder="Confirmer le mot de passe" id="confirmPassword" value={formData.confirmPassword} onChange={handleChange} />
+                            <input type="password" className="form-control " placeholder="Confirmer le mot de passe" id="confirmationMotDePasse" value={formData.confirmationMotDePasse} onChange={handleChange} />
                             <small className="text-light">*{errorMessage}</small>
                         </div>
 
